be2318eacb6a8d66fa0824063f2c7f11ce4acd02
ruby <ruby@lab01-30u.ftis.unpar>
2015-03-10 10:59:33 +0700

    melengkapi isi class Player

--- player.rb
+++ player.rb
@@ -1,9 +1,21 @@
 class Player
-  attr_accessor :nama, :blood, :manna
+  attr_accessor :name, :blood, :manna
   
-  def initialize nama
-    self.nama = nama
-    self.blood = 100
-    self.manna = 40
-    end
+  def initialize name
+    @name = name
+    @blood = 100
+    @manna = 40
+  end
+  
+  def isGameOver
+    @manna <= 0 || @blood <= 0
+  end
+  
+  def attack
+    @manna -= 20
+  end
+  
+  def defense
+    @blood -= 20
+  end
 end
